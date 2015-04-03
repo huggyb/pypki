4180985fd641cd719b0133e77f015d5938890609
huggy <huggy@asu.home.kich.land>
2015-04-03 16:09:35 +0200

    ssl.py

--- ssl.py
+++ ssl.py
@@ -9,39 +9,51 @@ from pprint import pprint
 from time import gmtime, mktime
 from os.path import exists, join
 
+def cert_request ():
+    """
+    Certificate request
+    Setup certificates attributes
+    return: cert object
+    """
+    cert = crypto.X509()
+    cert.get_subject().C = input('Country: ')
+    cert.get_subject().ST = input('State: ')
+    cert.get_subject().L = input('Location: ')
+    cert.get_subject().O = input('Organisation: ')
+    cert.get_subject().OU = input('Organisational unit: ')
+    cert.get_subject().CN = input('Common name: ')
+    cert.set_serial_number(1)
 
-CN = input('Common Name: ')
-CERT_FILE = "%s.crt" % CN
-KEY_FILE = "%s.key" % CN
+    # valid period
+    cert.gmtime_adj_notBefore(0)
+    cert.gmtime_adj_notAfter(315360000)
+    cert.set_issuer(cert.get_subject())
+    return cert
+
+PKI_DIR='key'
+
+
 
 # genkey
 k = crypto.PKey()
 k.generate_key(crypto.TYPE_RSA, 2048)
 
-# gencert
-cert = crypto.X509()
+mycert = cert_request()
 
-#cert attr
-cert.get_subject().C = input('Country: ')
-cert.get_subject().ST = input('State: ')
-cert.get_subject().L = input('Location: ')
-cert.get_subject().O = input('Organisation: ')
-cert.get_subject().OU = input('Organisational unit: ')
-cert.get_subject().CN = CN
+mycert.set_pubkey(k)
+mycert.sign( k, 'sha256' )
 
-cert.set_serial_number(1)
+mycn = mycert.get_subject().CN
 
-# valid period
-cert.gmtime_adj_notBefore(0)
-cert.gmtime_adj_notAfter(315360000)
+print(mycn)
 
-cert.set_issuer(cert.get_subject())
+open( PKI_DIR + "/" + mycn + ".crt", "wb" ).write( crypto.dump_certificate( crypto.FILETYPE_PEM, mycert ))
 
-cert.set_pubkey(k)
-cert.sign( k, 'sha256' )
+open ( PKI_DIR + "/" + mycn + ".key", "wb" ).write( crypto.dump_privatekey( crypto.FILETYPE_PEM, k ))
 
-open( CERT_FILE, "wb" ).write( crypto.dump_certificate( crypto.FILETYPE_PEM, cert ))
+# Function cert_request
+# Setup certificate attributes
 
-open ( KEY_FILE, "wb" ).write( crypto.dump_privatekey( crypto.FILETYPE_PEM, k ))
+
 
 
