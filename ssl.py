de9118fa94fadd742452833116bdfd12c24dbb53
huggy <huggy@asu.home.kich.land>
2015-04-03 20:32:59 +0200

    update ssl.py

--- ssl.py
+++ ssl.py
@@ -15,52 +15,84 @@ CA_CERT = 'cacert.crt'
 CA_KEY = 'cakey.key'
 PKI_DIR = 'key'
 
-# Function cert_request
-# Setup certificate attributes
-def cert_request ():
+# function cert_request
+# fetup certificate attributes
+def cert_request (list_attr):
     """
     Certificate request
     Setup certificates attributes
     return: cert object
     """
+
     cert = crypto.X509()
-    cert.get_subject().C = input('Country: ')
-    cert.get_subject().ST = input('State: ')
-    cert.get_subject().L = input('Location: ')
-    cert.get_subject().O = input('Organisation: ')
-    cert.get_subject().OU = input('Organisational unit: ')
-    cert.get_subject().CN = input('Common name: ')
+    cert.get_subject().C = list_attr[0]
+    cert.get_subject().ST = list_attr[1]
+    cert.get_subject().L = list_attr[2]
+    cert.get_subject().O = list_attr[3]
+    cert.get_subject().OU = list_attr[4]
+    cert.get_subject().CN = list_attr[5]
     cert.set_serial_number(1)
 
     # valid period
     cert.gmtime_adj_notBefore(0)
-    cert.gmtime_adj_notAfter(315360000)
+    cert.gmtime_adj_notAfter( 315360000 )
     cert.set_issuer(cert.get_subject())
     return cert
 
-def showhelp ():
-    print("usage: ssl.py [OPTIONS]")
-    print("\t-c --create\t-create clients certificates + key")
-    print("\t--genca\t\t-create new ca")
-    print("\t-h --help\t-print this menu")
+# function get_attr
+# ask user for certificates attributes
+def get_attr ():
+    """
+    Ask user for certificates attributes
+    Return list attr
+    """
+    attr = []
+    for i in  ( 'Country',  'State', 'Location', 'Organisation',
+            'Organizationnal Unit', 'Common Name' ):
+        a = input( i + ': ' )
+        attr += [ a ]
+    
+    return attr
 
-# Parse command line arguments
+
+# function showhelp
+# print help
+def showhelp ():
+    """
+    Print Help
+    """
+    print( 'usage: ssl.py [OPTIONS]' )
+    print( "\t-c --create\t-create clients certificates + key" )
+    print( "\t--defaultCA\t-create ROOT-CA with default param" )
+    print( "\t--genca\t\t-create new ca" )
+    print( "\t-h --help\t-print this menu" )
+
+
+
+CA = 0
+DEFCA = 0
+NEWREQ = 0
+# parse command line arguments
 if len(sys.argv) == 0:
-    print ('usage : ' + usage)
+    print ( 'usage : ' + usage )
     sys.exit(1)
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:],"ch:",["create","genca","help"])
+    opts, args = getopt.getopt( sys.argv[1:], 'ch:', [ 'create', 'defaultCA',
+            'genca', 'help' ])
 except getopt.GetoptError:
     showhelp()
     sys.exit(2)
 for opt, arg in opts:
-    if opt in ( "-c", "--create" ):
-        NEWREQ=1
-    elif opt in ( "--genca" ):
-        print( "Create new CA" )
-        CA=1
-    elif opt in ( "-h", "--help" ):
+    if opt in ( '-c', '--create' ):
+        NEWREQ = 1
+    if opt in ( '--defaultCA' ):
+        print( 'create default ROOT-CA' )
+        DEFCA = 1
+        CA = 1
+    elif opt in ( '--genca' ):
+        CA = 1
+    elif opt in ( '-h', '--help' ):
         showhelp()
         sys.exit(3)
     else:
@@ -68,21 +100,32 @@ for opt, arg in opts:
         sys.exit(5)
 
 
-# main 
+# main
 if CA == 1:
-    if os.path.isfile( PKI_DIR + '/' + CA_CERT ) and os.path.isfile( PKI_DIR + '/' + CA_KEY ):
+    if os.path.isfile( PKI_DIR + '/' + CA_CERT ) and os.path.isfile( 
+            PKI_DIR + '/' + CA_KEY ):
         print('There is an existing CA')
         
-    # genkey
-    k = crypto.PKey()
-    k.generate_key(crypto.TYPE_RSA, 2048)
+    print( 'Create new CA' )
 
-    mycert = cert_request()
-    mycert.set_pubkey(k)
-    mycert.sign( k, 'sha256' )
-    mycn = mycert.get_subject().CN
+    # get certificate attribues
+if DEFCA == 1:
+    my_attr = [ 'PL', 'Poneyland', 'kichland', 'Poney Corp', 'info',
+            'ROOT-CA Poney CORP' ]
+else:
+    my_attr = ( get_attr() )
     
-    open( PKI_DIR + '/' + CA_CERT, "wb" ).write( crypto.dump_certificate( crypto.FILETYPE_PEM, mycert ))
-    open ( PKI_DIR + '/' + CA_KEY, "wb" ).write( crypto.dump_privatekey( crypto.FILETYPE_PEM, k ))
+# genkey
+k = crypto.PKey()
+k.generate_key( crypto.TYPE_RSA, 2048 )
 
 
+my_cert = cert_request( my_attr )
+my_cert.set_pubkey(k)
+my_cert.sign( k, 'sha256' )
+my_cn = my_cert.get_subject().CN
+
+open( PKI_DIR + '/' + CA_CERT, 'wb' ).write( crypto.dump_certificate(
+    crypto.FILETYPE_PEM, my_cert ))
+open ( PKI_DIR + '/' + CA_KEY, 'wb' ).write( crypto.dump_privatekey(
+    crypto.FILETYPE_PEM, k ))
