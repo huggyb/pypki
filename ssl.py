f84a9c489c5d3eeede1f9a9b33d55df67734cd08
huggyb <huggy@kich.land>
2015-04-19 15:14:43 +0200

    add client certificate request

--- ssl.py
+++ ssl.py
@@ -16,7 +16,7 @@ CA_CERT_FULLPATH = os.path.join(PKI_DIR, CA_CERT)
 CA_KEY_FULLPATH = os.path.join(PKI_DIR, CA_KEY)
 
 
-def cert_request(list_attr):
+def cacert_req(list_attr):
     """
     Certificate request
     Setup certificates attributes
@@ -38,6 +38,27 @@ def cert_request(list_attr):
     cert.set_issuer(cert.get_subject())
     return cert
 
+def csr_req(list_attr):
+    """
+    Certificate request
+    Setup certificates attributes
+    return: cert object
+    """
+    cert = crypto.X509()
+    cert.get_subject().C = list_attr[0]
+    cert.get_subject().ST = list_attr[1]
+    cert.get_subject().L = list_attr[2]
+    cert.get_subject().O = list_attr[3]
+    cert.get_subject().OU = list_attr[4]
+    cert.get_subject().CN = list_attr[5]
+    cert.set_serial_number(1)
+
+    # valid period
+
+    cert.gmtime_adj_notBefore(0)
+    cert.gmtime_adj_notAfter(315360000)
+
+    return cert
 
 def get_attr():
     """
@@ -46,7 +67,7 @@ def get_attr():
     """
     attr = []
     for i in ('Country', 'State', 'Location', 'Organisation', 'Organizationnal Unit', 'Common Name'):
-        a = input(i + ': ')
+        a = raw_input(i + ': ')
         attr += [a]
     return attr
 
@@ -77,7 +98,7 @@ def main():
         k = crypto.PKey()
         k.generate_key(crypto.TYPE_RSA, 2048)
 
-        my_cert = cert_request(default_attrs)
+        my_cert = cacert_req(default_attrs)
         my_cert.set_pubkey(k)
         my_cert.sign(k, 'sha256')
 
@@ -85,6 +106,39 @@ def main():
             fd.write(crypto.dump_certificate(crypto.FILETYPE_PEM, my_cert))
         with open(CA_KEY_FULLPATH, 'wb') as fd:
             fd.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, k))
+    elif args.create:
+        # load ROOT CA private key
+        ca_key_fd = open(CA_KEY_FULLPATH, 'rb').read()
+        ca_key = crypto.load_privatekey(crypto.FILETYPE_PEM, ca_key_fd )
+        ca_cert_fd = open(CA_CERT_FULLPATH, 'rb').read()
+        ca_cert = crypto.load_certificate(crypto.FILETYPE_PEM, ca_cert_fd )
+        # create csr 
+        cli_pkey = crypto.PKey()
+        cli_pkey.generate_key(crypto.TYPE_RSA, 2048)
+        cli_attrs = get_attr()
+        cli_csr = csr_req( cli_attrs )
+        cli_csr.set_pubkey(cli_pkey)
+        cli_csr.sign( cli_pkey, 'sha256')
+
+        cli_cert = crypto.X509()
+        cli_cert.set_issuer(ca_cert.get_subject())
+        cli_cert.gmtime_adj_notBefore(0)
+        cli_cert.gmtime_adj_notAfter(315360000)
+        cli_cert.set_subject(cli_csr.get_subject())
+        cli_cert.set_pubkey(cli_csr.get_pubkey())
+        cli_cert.sign( ca_key, 'sha256')
+        with open('key/client.crt', 'wb') as fd:
+            fd.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cli_cert))
+        with open('key/client.key', 'wb') as fd:
+            fd.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, cli_pkey))
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     main()
